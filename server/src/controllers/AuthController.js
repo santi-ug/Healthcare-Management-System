cb1210062d1c2f3920bb828968c07e302e441016
santi-ug <santi.uribegil@gmail.com>
2025-03-18 19:30:02 -0500

    feature: created auth routes and controller, as well as the main router

---
+++ server/src/controllers/AuthController.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const signup = async (req, res) => {
+}
+
+export const login = async (req, res) => {
+
+}
